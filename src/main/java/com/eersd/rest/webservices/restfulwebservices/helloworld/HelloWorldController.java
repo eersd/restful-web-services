bb2a0615928be12d3729e263ce695f7028e13435
Erick Ersada <erick.ersada@prodyna.com>
2022-06-12 23:05:02 +0200

    added ErrorHandling, internalization and OpenAPI Spec

--- src/main/java/com/eersd/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/eersd/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.eersd.rest.webservices.restfulwebservices.helloworld;
 
 import com.eersd.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private MessageSource messageSource;
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "hello world";
@@ -22,5 +30,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPath(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(
+        //@RequestHeader(name="Accept-Language", required = false) Locale locale
+    ){
+        //return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
     }
 }
--- src/main/java/com/eersd/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/eersd/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,19 @@
 package com.eersd.rest.webservices.restfulwebservices.user;
 
+import com.eersd.rest.webservices.restfulwebservices.exception.ExceptionResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -18,12 +28,32 @@ public class UserResource {
 
     //retrieveUser(int id)
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
-        return service.findOne(id);
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null)
+            throw new ExceptionResponse.UserNotFoundException("id-"+id);
+        EntityModel<User> model = EntityModel.of(user);
+
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
+        //return service.findOne(id);
+
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user){
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user){
+
         User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        User user = service.deleteById(id);
+        if(user == null){
+            throw new ExceptionResponse.UserNotFoundException("id-"+id);
+        }
     }
 }
